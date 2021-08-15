13d54492c171a208957966d0f16bb3c068cde95d
Junichi Nishi <github@purewest.dip.jp>
2021-08-15 23:19:27 +0900

    settings.pyを修正

--- RareTECH_Love/settings.py
+++ RareTECH_Love/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yh9ibnng^_i@4n@=y25pz$%p3#oe!$+xz23g#l_1*kq#2kdvfa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'RareTECH_Love.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
